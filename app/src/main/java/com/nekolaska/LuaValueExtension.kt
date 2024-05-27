b2190d36cb71d5263c7741b3f738444dd7672a39
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-05-27 19:35:53 +0800

    improve file list performance

---
+++ app/src/main/java/com/nekolaska/LuaValueExtension.kt
@@ -0,0 +1,20 @@
+package com.nekolaska
+
+import org.luaj.LuaValue
+import org.luaj.Varargs
+import org.luaj.lib.jse.CoerceJavaToLua
+
+inline fun Varargs.firstArg(): LuaValue = this.arg1()
+inline fun Varargs.secondArg(): LuaValue = this.arg(2)
+inline fun Varargs.argAt(index: Int): LuaValue = this.arg(index)
+inline fun Varargs.asString(): String = this.tojstring()
+inline fun LuaValue.isNotNil(): Boolean = !this.isnil()
+
+inline fun LuaValue.ifNotNil(): LuaValue? = takeIf { it.isNotNil() }
+inline fun LuaValue.ifIsFunction(): LuaValue? = takeIf { it.isfunction() }
+
+inline fun <T> T.toLuaValue(): LuaValue = CoerceJavaToLua.coerce(this)
+
+//inline fun <reified T : Any> KClass<T>.toLuaClass(): LuaValue = CoerceJavaToLua.coerce(this.java)
+inline fun <reified T> Array<T>.toVarargs(): Varargs = LuaValue.varargsOf(this)
+inline fun <reified T> ArrayList<T>.toVarargs(): Varargs = LuaValue.varargsOf(this)
