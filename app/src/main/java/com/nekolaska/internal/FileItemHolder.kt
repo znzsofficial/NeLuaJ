12e011a63bb26c649b2bb81bb004d9ac2bc681e4
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-05-28 10:05:11 +0800

    improve file list performance

--- app/src/main/java/com/nekolaska/internal/FileItemHolder.kt
+++ app/src/main/java/com/nekolaska/internal/FileItemHolder.kt
@@ -5,19 +5,24 @@ import com.nekolaska.toLuaValue
 import github.znzsofficial.adapter.LuaCustRecyclerHolder
 import github.znzsofficial.neluaj.R
 import org.luaj.LuaTable
+import org.luaj.LuaValue
 
 class FileItemHolder(itemView: View) : LuaCustRecyclerHolder(itemView) {
+    val views = LuaTable()
 
     fun bind(): LuaTable {
-        views = LuaTable().apply {
+        return views.apply {
             set("contents", itemView.findViewById<View>(R.id.contents).toLuaValue())
             set("name", itemView.findViewById<View>(R.id.name).toLuaValue())
             set("icon", itemView.findViewById<View>(R.id.icon).toLuaValue())
         }
-        return views as LuaTable
     }
 
     fun unbind() {
-        views = null
+        views.apply {
+            set("contents", LuaValue.NIL)
+            set("name", LuaValue.NIL)
+            set("icon", LuaValue.NIL)
+        }
     }
 }
