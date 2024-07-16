aa35df2138244b706b96fcdfe2e0e00e57de8921
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-07-16 22:38:08 +0800

    update

--- app/src/main/java/com/nekolaska/internal/MyFileTabView.kt
+++ app/src/main/java/com/nekolaska/internal/MyFileTabView.kt
@@ -1,12 +1,11 @@
 package com.nekolaska.internal
 
 import android.content.Context
-import android.util.AttributeSet
 import android.view.MotionEvent
 import github.daisukiKaffuChino.LuaFileTabView
 
-class MyFileTabView(context: Context?, attrs: AttributeSet?) : LuaFileTabView(context, attrs) {
-   override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
-       return true
-   }
+class MyFileTabView(context: Context) : LuaFileTabView(context) {
+    override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
+        return true
+    }
 }
