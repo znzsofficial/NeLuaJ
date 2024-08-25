0d21105565c70d82d6aa3b3ccb99300ddaf7511f
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-08-25 20:41:40 +0800

    refactor(utils): 重构LuaFileUtil并更新相关模块
    -重构LuaFileUtil模块，实现文件操作的NIO和Okio实现分离。
    - 更新TabUtil模块，优化弹出菜单的事件监听和项目路径处理。
    - 在环境检测中移除NIO支持的检测，简化代码结构。
    -调整EditorUtil和其他相关模块，适应新的文件操作实现。
    -重构环境变量配置，统一文件备份检查逻辑。

--- app/src/main/assets/activities/java/JavaEditorActivity.lua
+++ app/src/main/assets/activities/java/JavaEditorActivity.lua
@@ -1,42 +1,41 @@
 require "environment"
 local ColorUtil = this.globalData.ColorUtil
-import "mods.utils.LuaFileUtil"
-import "java.lang.Runnable"
+local LuaFileUtil = luajava.bindClass "com.nekolaska.io.LuaFileUtil".INSTANCE
 import "res"
 local File = bindClass "java.io.File"
 local path = tostring(...)
 activity.setContentView(res.layout.java_layout)
 
 function onCreateOptionsMenu(menu)
-  local ColorTitle = ColorUtil.getColorOnBackground();
-  local menu_show = 2; --MenuItem.SHOW_AS_ACTION_ALWAYS;
-  menu.add("撤销")
-  .setShowAsAction(menu_show)
-  .setIcon(DrawableUtil.getDrawable("undo", ColorTitle))
-  .onMenuItemClick=function(a)
-    mEditor.undo()
-  end
-  menu.add("重做")
-  .setShowAsAction(menu_show)
-  .setIcon(DrawableUtil.getDrawable("redo", ColorTitle))
-  .onMenuItemClick=function(a)
-    mEditor.redo()
-  end
-  --[[
-  menu.add("保存")
-  .setShowAsAction(menu_show)
-  .onMenuItemClick=function(a)
-    LuaFileUtil.write(path, tostring(mEditor.getText()))
-  end]]
+    local ColorTitle = ColorUtil.getColorOnBackground();
+    local menu_show = 2; --MenuItem.SHOW_AS_ACTION_ALWAYS;
+    menu.add("撤销")
+        .setShowAsAction(menu_show)
+        .setIcon(DrawableUtil.getDrawable("undo", ColorTitle))
+        .onMenuItemClick = function(a)
+        mEditor.undo()
+    end
+    menu.add("重做")
+        .setShowAsAction(menu_show)
+        .setIcon(DrawableUtil.getDrawable("redo", ColorTitle))
+        .onMenuItemClick = function(a)
+        mEditor.redo()
+    end
+    --[[
+    menu.add("保存")
+    .setShowAsAction(menu_show)
+    .onMenuItemClick=function(a)
+      LuaFileUtil.write(path, tostring(mEditor.getText()))
+    end]]
 end
 
 mEditor.post(function()
-  if File(path).exists()
-    mEditor.setText(LuaFileUtil.read(path))
-    activity.setTitle(File(path).getName())
-   else
-    mEditor.setText("")
-    activity.setTitle(res.string.nofile)
-  end
-  mEditor.setTypeface(res.font.code)
+    if File(path).exists() then
+        mEditor.setText(LuaFileUtil.read(path))
+        activity.setTitle(File(path).getName())
+    else
+        mEditor.setText("")
+        activity.setTitle(res.string.nofile)
+    end
+    mEditor.setTypeface(res.font.code)
 end)
--- app/src/main/assets/environment.lua
+++ app/src/main/assets/environment.lua
@@ -1,6 +1,7 @@
 bindClass = luajava.bindClass;
+local File = bindClass "java.io.File"
 
-for _,v in (luajava.newInstance("java.io.File",this.getLuaDir("libs/")).listFiles()) do
+for _,v in File(this.getLuaDir("libs/")).listFiles() do
   v.setReadOnly()
 end
 
@@ -11,12 +12,6 @@ Compat_R = bindClass "androidx.appcompat.R"
 
 SupportProperties = require "mods.property.SupportProperties"
 
-try
-  bindClass "java.nio.file.Files";
-  catch
-  SupportProperties.NIO = false
-end
-
 MainActivity={}
 MainActivity.Public=require"activities.main.MainActivity$1"
 MainActivity.RecyclerView=require"activities.main.MainActivity$RecyclerView"
@@ -24,6 +19,14 @@ Bean={}
 Bean.Path = require "mods.bean.PathBean"
 Bean.Project = require "mods.bean.ProjectBean"
 
+checkBackup = function()
+  local p = luajava.astable(activity.getExternalMediaDirs())[1].getPath()
+  Bean.Path.backup_dir = p.."/backups"
+  local backup = File(Bean.Path.backup_dir.."/"..os.date("%Y-%m-%d"));
+  if not backup.exists() then
+    backup.mkdirs();
+  end
+end
+LuaFileUtil = bindClass "com.nekolaska.io.LuaFileUtil".INSTANCE
 PathManager = require "mods.utils.PathManager"
-LuaFileUtil = require "mods.utils.LuaFileUtil"
 DrawableUtil = require "mods.utils.DrawableUtil"
