7ea567207d26929c2bd09fe06f7acd89a829148d
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-08-27 21:27:30 +0800

    refactor(file-item): 重构FileItemHolder并更新相关资源及活动
    - 重构FileItemHolder类，更改绑定和解绑逻辑。- 更新Lua活动中的函数名称以遵循命名约定。
    - 修改item_file.xml中的视图ID以进行一致性检查。
    - 从PublicClasses.lua中移除过时的导入语句。

--- app/src/main/java/com/nekolaska/internal/FileItemHolder.kt
+++ app/src/main/java/com/nekolaska/internal/FileItemHolder.kt
@@ -5,24 +5,18 @@ import com.nekolaska.toLuaValue
 import github.znzsofficial.adapter.LuaCustRecyclerHolder
 import github.znzsofficial.neluaj.R
 import org.luaj.LuaTable
-import org.luaj.LuaValue
 
 class FileItemHolder(itemView: View) : LuaCustRecyclerHolder(itemView) {
     val Tag = LuaTable()
     fun bind(): LuaTable {
         return Tag.apply {
-            set("contents", itemView.findViewById<View>(R.id.contents).toLuaValue())
-            set("name", itemView.findViewById<View>(R.id.name).toLuaValue())
-            set("icon", itemView.findViewById<View>(R.id.icon).toLuaValue())
+            set("contents", itemView.findViewById<View>(R.id.item_contents).toLuaValue())
+            set("name", itemView.findViewById<View>(R.id.item_name).toLuaValue())
+            set("icon", itemView.findViewById<View>(R.id.item_icon).toLuaValue())
         }
     }
 
-
     fun unbind() {
-        Tag.apply {
-            set("contents", LuaValue.NIL)
-            set("name", LuaValue.NIL)
-            set("icon", LuaValue.NIL)
-        }
+        Tag.clear()
     }
 }
