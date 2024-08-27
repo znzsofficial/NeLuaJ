b228160c25b100a8e5ed34dfbb226f072201abfb
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-08-27 21:02:06 +0800

    refactor(utils): 使用环境文件中的方法获取Drawable，优化资源访问- 从环境文件中引入新方法getResDrawable，优化资源访问方式。 - 移除DrawableUtil模块，简化代码结构。 - 更新相关活动和布局文件，使用新方法获取Drawable资源。 - 修正FixActivity中的方法命名以遵循Lua命名约定。 - 移除SupportProperties

--- app/src/main/assets/activities/photo/PhotoActivity.lua
+++ app/src/main/assets/activities/photo/PhotoActivity.lua
@@ -2,50 +2,55 @@ require "environment"
 import "android.view.View"
 import "android.view.WindowManager"
 import "android.graphics.Color"
-import "coil.Coil"
-import "coil.request.ImageRequest"
+import "com.androlua.LuaTarget"
+local ImageRequestBuilder = bindClass "coil.request.ImageRequest$Builder"
 
 activity.getSupportActionBar().hide()
 
 local path = ...
 
-try
-  local window = activity.getWindow()
-  window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-  window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-  | View.SYSTEM_UI_FLAG_LAYOUT_STABLE |View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-  window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-  window.setStatusBarColor(Color.TRANSPARENT);
-  window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-end
+pcall(function()
+    local window = activity.getWindow()
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.setStatusBarColor(Color.TRANSPARENT);
+    window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+end)
 
 local binding = {}
-activity.setContentView(loadlayout(res.layout.photo_layout,binding))
+activity.setContentView(loadlayout(res.layout.photo_layout, binding))
 
-local imageLoader = Coil.imageLoader(activity)
+local imageLoader = this.getImageLoader()
 
 imageLoader.enqueue(
-ImageRequest.Builder(this)
-.data(DrawableUtil.getDrawable("sync", this.globalData.ColorUtil.getColorOnPrimaryContainer()))
-.target(binding.switchBg).build()
+        ImageRequestBuilder(this)
+                     .data(this.getLuaDir() .. "/res/drawable/sync.png")
+                     .target(LuaTarget(LuaTarget.Listener {
+            onSuccess = function(drawable)
+                drawable.setColorFilter(this.getFilter(this.globalData.ColorUtil.getColorOnPrimaryContainer()))
+                binding.switchBg.setImageDrawable(drawable)
+            end
+        }))          .build()
 )
 
 imageLoader.enqueue(
-ImageRequest.Builder(this)
-.data(path)
-.target(binding.mPhotoView).build()
+        ImageRequestBuilder(this)
+                .data(path)
+                .target(binding.mPhotoView).build()
 )
 
 local i = 1
 local colors = {
-  0xff888888,
-  0xff363636,
-  0xffcccccc,
-  0xffffffff,
-  0xff000000,
+    0xff888888,
+    0xff363636,
+    0xffcccccc,
+    0xffffffff,
+    0xff000000,
 }
 
 binding.switchBg.onClick = function()
-  binding.bg.setBackgroundColor(colors[i])
-  i = i % #colors + 1
+    binding.bg.setBackgroundColor(colors[i])
+    i = i % #colors + 1
 end
