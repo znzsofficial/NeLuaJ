f154466edccc39641d611d3ea374e39a06d1846e
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-10-14 11:13:08 +0800

    docs(lua): update documentation for getFilter function
    - Update the LuaActivity.md file to include information about the getFilter function
    - Add mention of BlendModeColorFilter alongside PorterDuffColorFilter
    - Improve documentation for better clarity and completeness

--- app/src/main/java/org/luaj/android/xTask.kt
+++ app/src/main/java/org/luaj/android/xTask.kt
@@ -3,9 +3,11 @@ package org.luaj.android
 import androidx.lifecycle.lifecycleScope
 import com.androlua.LuaActivity
 import com.androlua.LuaGcable
+import com.nekolaska.argAt
 import com.nekolaska.asString
 import com.nekolaska.firstArg
 import com.nekolaska.ifIsFunction
+import com.nekolaska.secondArg
 import com.nekolaska.toLuaValue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -21,22 +23,22 @@ class xTask(private val mContext: LuaActivity) : VarArgFunction(), LuaGcable {
     private var job: Job? = null
 
     override fun invoke(args: Varargs): Varargs =
-        args.firstArg().checktable().let { table ->
-            mContext.lifecycleScope.launch {
+        args.firstArg().run {
+            if (isfunction()) mContext.lifecycleScope.launch {
                 withContext(
-                    when (table["dispatcher"].asString()) {
+                    when (args.argAt(3).asString()) {
                         "io" -> Dispatchers.IO
                         else -> Dispatchers.Default
                     }
                 ) {
                     try {
-                        table["task"].ifIsFunction()?.call(coroutineContext.toLuaValue())
+                        call(coroutineContext.toLuaValue())
                     } catch (e: LuaError) {
                         mContext.sendError("xTask: Background", e)
                         NIL
                     }
                 }.let { result ->
-                    table["callback"].ifIsFunction()?.apply {
+                    args.secondArg().ifIsFunction()?.apply {
                         runCatching {
                             invoke(result)
                         }.onFailure {
@@ -46,8 +48,33 @@ class xTask(private val mContext: LuaActivity) : VarArgFunction(), LuaGcable {
                 }
                 // 先把 job 存起来，再用 let 返回
             }.also { job = it }.let { job.toLuaValue() }
+            else checktable().let { table ->
+                mContext.lifecycleScope.launch {
+                    withContext(
+                        when (table["dispatcher"].asString()) {
+                            "io" -> Dispatchers.IO
+                            else -> Dispatchers.Default
+                        }
+                    ) {
+                        try {
+                            table["task"].ifIsFunction()?.call(coroutineContext.toLuaValue())
+                        } catch (e: LuaError) {
+                            mContext.sendError("xTask: Background", e)
+                            NIL
+                        }
+                    }.let { result ->
+                        table["callback"].ifIsFunction()?.apply {
+                            runCatching {
+                                invoke(result)
+                            }.onFailure {
+                                mContext.sendError("xTask: Main", it as LuaError)
+                            }
+                        }
+                    }
+                    // 先把 job 存起来，再用 let 返回
+                }.also { job = it }.let { job.toLuaValue() }
+            }
         }
-
 
     override fun gc() {
         job?.cancel()
