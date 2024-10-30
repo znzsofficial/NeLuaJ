2bd89b905bb7329c2f596db5c3609a3cc4b193a8
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-10-30 21:56:57 +0800

    feat(app): update target SDK
    - Update targetSdk from 33 to 34 in app/build.gradle.kts
    - Modified the behavior of DexMaker to adapt to the requirements of API 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "github.znzsofficial.neluaj"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/androlua/LuaEnhancer.java
@@ -0,0 +1,75 @@
+package com.androlua;
+
+import org.luaj.LuaValue;
+
+import java.lang.reflect.Field;
+
+import dx.proxy.Enhancer;
+import dx.proxy.EnhancerInterface;
+import dx.proxy.MethodFilter;
+import dx.proxy.MethodInterceptor;
+
+/**
+ * Created by nirenr on 2018/12/19.
+ */
+
+public class LuaEnhancer {
+
+    private final Enhancer mEnhancer;
+
+    public LuaEnhancer(String cls) throws ClassNotFoundException {
+        this(Class.forName(cls));
+    }
+
+    public LuaEnhancer(Class<?> cls) {
+        mEnhancer = new Enhancer(LuaApplication.getInstance());
+        mEnhancer.setSuperclass(cls);
+    }
+
+    public void setInterceptor(EnhancerInterface obj, MethodInterceptor interceptor) {
+        obj.setMethodInterceptor_Enhancer(interceptor);
+    }
+
+    public static void setInterceptor(Class obj, MethodInterceptor interceptor) {
+        try {
+            Field field = obj.getDeclaredField("methodInterceptor");
+            field.setAccessible(true);
+            field.set(obj, interceptor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Class<?> create() {
+        try {
+            return mEnhancer.create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Class<?> create(MethodFilter filer) {
+        try {
+            mEnhancer.setMethodFilter(filer);
+            return mEnhancer.create();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Class<?> create(LuaValue arg) {
+        MethodFilter filter = (method, name) -> !arg.get(name).isnil();
+        try {
+            mEnhancer.setMethodFilter(filter);
+            Class<?> cls = mEnhancer.create();
+            setInterceptor(cls, new LuaMethodInterceptor(arg));
+            return cls;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
