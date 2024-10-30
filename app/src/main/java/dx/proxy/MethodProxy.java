81c8ae5f3ad3cff61912227ae249d0eaef2e15b4
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-10-30 21:56:57 +0800

    feat(app): update target SDK
    - Update targetSdk from 33 to 34 in app/build.gradle.kts
    - Modified the behavior of DexMaker to adapt to the requirements of API 34

---
+++ app/src/main/java/dx/proxy/MethodProxy.java
@@ -0,0 +1,67 @@
+package dx.proxy;
+
+import java.lang.reflect.Method;
+
+public class MethodProxy {
+
+    private final Class subClass;
+    private final String methodName;
+    private final Class[] argsType;
+
+    @SuppressWarnings("rawtypes")
+    public MethodProxy(Class subClass, String methodName, Class[] argsType) {
+        this.subClass = subClass;
+        this.methodName = methodName;
+        this.argsType = argsType;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Method getOriginalMethod() {
+        try {
+            return subClass.getMethod(methodName, argsType);
+        } catch (NoSuchMethodException e) {
+            try {
+                return subClass.getDeclaredMethod(methodName, argsType);
+            } catch (NoSuchMethodException e2) {
+                throw new ProxyException(e2.getMessage());
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Method getSuperMethod() {
+        try {
+            return subClass.getSuperclass().getMethod(methodName, argsType);
+        } catch (NoSuchMethodException e) {
+            try {
+                return subClass.getSuperclass().getDeclaredMethod(methodName, argsType);
+            } catch (NoSuchMethodException e2) {
+                throw new ProxyException(e2.getMessage());
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Method getProxyMethod() {
+        try {
+            return subClass.getMethod(methodName + Const.SUBCLASS_INVOKE_SUPER_SUFFIX, argsType);
+        } catch (NoSuchMethodException e) {
+            throw new ProxyException(e.getMessage());
+        }
+    }
+
+    public Object invokeSuper(Object object, Object[] argsValue) {
+        return ((EnhancerInterface) object).executeSuperMethod_Enhancer(methodName, argsType, argsValue);
+    }
+
+    @Override
+    public String toString() {
+        return "MethodProxy{" +
+                getSuperMethod() +
+                '}';
+    }
+}
