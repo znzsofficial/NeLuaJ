b097329c3cdaf52af7d2bb6528977a3ac665de9a
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2024-12-05 22:05:21 +0800

    feat(LuaPagerAdapter): improve functionality and handle null safely
    - Add null check for titles list to avoid NullPointerException
    - Implement missing override annotations for overridden methods- Add support for adding and inserting items with titles
    - Update remove methods to handle title removal and notify dataset change- Refactor some method names to match standard naming conventions

--- app/src/main/java/github/daisukiKaffuChino/LuaPagerAdapter.java
+++ app/src/main/java/github/daisukiKaffuChino/LuaPagerAdapter.java
@@ -1,7 +1,5 @@
 package github.daisukiKaffuChino;
 
-import android.database.DataSetObservable;
-import android.database.DataSetObserver;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,10 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager.widget.PagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class LuaPagerAdapter extends PagerAdapter {
-    private final DataSetObservable mObservable = new DataSetObservable();
     List<View> pagerViews;
     List<String> titles;
 
@@ -22,63 +20,77 @@ public final class LuaPagerAdapter extends PagerAdapter {
 
     public LuaPagerAdapter(List<View> list, List<String> titles) {
         this.pagerViews = list;
-        this.titles = titles;
+        this.titles = titles != null ? titles : new ArrayList<>(); // 避免 titles 为 null
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        if (titles != null) {
+        if (titles != null && position < titles.size()) {
             return titles.get(position);
         } else {
             return "No Title";
         }
     }
 
-    public void destroyItem(@NonNull ViewGroup viewGroup, int i, @NonNull Object obj) {
-        viewGroup.removeView(this.pagerViews.get(i));
+    @Override
+    public void destroyItem(@NonNull ViewGroup viewGroup, int position, @NonNull Object object) {
+        viewGroup.removeView(this.pagerViews.get(position));
     }
 
+    @Override
     public int getCount() {
         return this.pagerViews.size();
     }
 
     @NonNull
-    public Object instantiateItem(@NonNull ViewGroup viewGroup, int i) {
-        viewGroup.addView(this.pagerViews.get(i));
-        return this.pagerViews.get(i);
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup viewGroup, int position) {
+        viewGroup.addView(this.pagerViews.get(position));
+        return this.pagerViews.get(position);
     }
 
+    @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
     }
 
-    public void notifyDataSetChanged() {
-        mObservable.notifyChanged();
-    }
-
-    public void registerDataSetObserver(@NonNull DataSetObserver observer) {
-        mObservable.registerObserver(observer);
-    }
-
-    public void unregisterDataSetObserver(@NonNull DataSetObserver observer) {
-        mObservable.unregisterObserver(observer);
-    }
 
     public void add(View view) {
         pagerViews.add(view);
+        notifyDataSetChanged();  // 通知数据已更改
+    }
+
+    public void add(View view, String title) {
+        pagerViews.add(view);
+        titles.add(title);
+        notifyDataSetChanged();  // 通知数据已更改
     }
 
     public void insert(int index, View view) {
         pagerViews.add(index, view);
+        notifyDataSetChanged();  // 通知数据已更改
+    }
+
+    public void insert(int index, View view, String title) {
+        pagerViews.add(index, view);
+        titles.add(index, title);
+        notifyDataSetChanged();  // 通知数据已更改
     }
 
     public View remove(int index) {
-        return pagerViews.remove(index);
+        View removedView = pagerViews.remove(index);
+        if (titles != null) titles.remove(index);
+        notifyDataSetChanged();  // 通知数据已更改
+        return removedView;
     }
 
     public boolean remove(View view) {
-        return pagerViews.remove(view);
+        boolean removed = pagerViews.remove(view);
+        if (removed) {
+            notifyDataSetChanged();  // 通知数据已更改
+        }
+        return removed;
     }
 
     public View getItem(int index) {
