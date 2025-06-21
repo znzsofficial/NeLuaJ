82a6cc9e7832c5ba49903c58dcca537bd7a1dc0c
znzsofficial <63937015+znzsofficial@users.noreply.github.com>
2025-06-21 14:36:30 +0800

    feat(internal): rewrite CustomMagnifier for improved performance and flexibility
    - Improve positioning logic for the magnifier card
    - Update dependencies:
      - lottie-android from 6.6.6 to 6.6.7

--- app/src/main/java/com/nekolaska/internal/CustomMagnifier.kt
+++ app/src/main/java/com/nekolaska/internal/CustomMagnifier.kt
@@ -2,93 +2,82 @@ package com.nekolaska.internal
 
 import android.app.Activity
 import android.graphics.Bitmap
-import android.graphics.Matrix
-import android.util.DisplayMetrics
-import android.util.TypedValue
+import android.graphics.Rect
+import android.os.Handler
+import android.os.Looper
+import android.view.PixelCopy
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.cardview.widget.CardView
+import androidx.core.util.TypedValueCompat
+import androidx.core.graphics.createBitmap
 
-@Suppress("DEPRECATION")
-class CustomMagnifier(private val mView: View) {
-    private var imageHeight: Float
-    private var imageWidth: Float
+class CustomMagnifier(private val sourceView: View) {
+    private val context = sourceView.context
+    private val displayMetrics = context.resources.displayMetrics
+    private val mainHandler = Handler(Looper.getMainLooper())
+
+    // The window is required for PixelCopy
+    private val window: Window? = (context as Activity).window
+    // UI Elements
+    private val magnifierCard: CardView
+    private val magnifierImage: AppCompatImageView
+
+    // The root view to which the magnifier will be added as an overlay.
+    private val parentView = sourceView.rootView as? ViewGroup
+
+    // Properties
+    private var lastBitmap: Bitmap? = null
     private var lastSourceCenterX = 0f
     private var lastSourceCenterY = 0f
-    private val mCardView: CardView
-    private val mContext: Activity = mView.context as Activity
-    private val mDecorView: FrameLayout
-    private val displayMetrics: DisplayMetrics = mContext.resources.displayMetrics
-    private val mImageView: AppCompatImageView
-    private var mLastBitmap: Bitmap? = null
-    private val matrix = Matrix()
-    private var zoom = 1.25f
-    private val verticalOffset: Float
-    private var mElevation = 4.0f
+
+    // Configurable dimensions and style
+    var zoom: Float = 1.25f
+    var cornerRadius: Float
+        get() = magnifierCard.radius
+        set(value) {
+            magnifierCard.radius = value
+        }
+    var elevationInDp: Float = 4.0f
+        set(value) {
+            field = value
+            magnifierCard.cardElevation = TypedValueCompat.dpToPx(value,displayMetrics)
+        }
+
+    private var magnifierWidth = TypedValueCompat.dpToPx(100.0f,displayMetrics)
+    private var magnifierHeight = TypedValueCompat.dpToPx(48.0f,displayMetrics)
+    private var verticalOffset = TypedValueCompat.dpToPx(-42.0f,displayMetrics)
 
     init {
-        mCardView = CardView(mContext)
-        mImageView = AppCompatImageView(mContext)
-        mCardView.addView(mImageView)
-        mCardView.visibility = View.GONE
-        // 设置卡片圆角
-        mCardView.radius = 16f
-        mCardView.cardElevation = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            mElevation,
-            displayMetrics
-        )
-        val decorView = mContext.window.decorView as FrameLayout
-        mDecorView = decorView
-        decorView.addView(mCardView)
-        imageWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100.0f, displayMetrics)
-        imageHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48.0f, displayMetrics)
-        verticalOffset =
-            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, -42.0f, displayMetrics)
-        val layoutParams = mCardView.layoutParams
-        layoutParams.width = imageWidth.toInt()
-        layoutParams.height = imageHeight.toInt()
-        mCardView.layoutParams = layoutParams
-    }
-
-    private fun cropBitmap(bitmap: Bitmap, x: Int, y: Int, width: Int, height: Int): Bitmap {
-        var x = x
-        var y = y
-        val scale = zoom
-        matrix.setScale(scale, scale)
-        if (x < 0) {
-            x = 0
-        } else if (x > bitmap.width - width) {
-            x = bitmap.width - width
+        // Fail fast if the magnifier cannot be created.
+        if (window == null) {
+            throw IllegalStateException("The source view's context is not an Activity or it has no window.")
         }
-        if (y < 0) {
-            y = 0
-        } else if (y > bitmap.height - height) {
-            y = bitmap.height - height
+        if (parentView == null) {
+            throw IllegalStateException("The source view is not attached to a window, or its root view is not a ViewGroup.")
         }
-        val croppedBitmap = Bitmap.createBitmap(bitmap, x, y, width, height, matrix, true)
-        bitmap.recycle()
-        return croppedBitmap
-    }
 
-    fun dismiss() {
-        mCardView.visibility = View.GONE
-        val bitmap = mLastBitmap
-        bitmap?.recycle()
-    }
+        magnifierImage = AppCompatImageView(context).apply {
+            scaleType = ImageView.ScaleType.FIT_XY
+        }
 
-    fun setDimensions(width: Float, height: Float) {
-        imageWidth = width
-        imageHeight = height
-        val layoutParams = mCardView.layoutParams
-        layoutParams.width = imageWidth.toInt()
-        layoutParams.height = imageHeight.toInt()
-        mCardView.layoutParams = layoutParams
-    }
+        magnifierCard = CardView(context).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                magnifierWidth.toInt(),
+                magnifierHeight.toInt()
+            )
+            radius = TypedValueCompat.dpToPx(16f,displayMetrics) // Default corner radius
+            cardElevation = TypedValueCompat.dpToPx(elevationInDp,displayMetrics)
+            visibility = View.GONE
+            addView(magnifierImage)
+        }
 
-    fun setCornerRadius(radius: Float) {
-        mCardView.radius = radius
+        // Add the magnifier to the top-level container
+        parentView.addView(magnifierCard)
     }
 
     @JvmOverloads
@@ -96,74 +85,80 @@ class CustomMagnifier(private val mView: View) {
         sourceX: Float,
         sourceY: Float,
         destinationX: Float = sourceX,
-        destinationY: Float = verticalOffset + sourceY
+        destinationY: Float = sourceY + verticalOffset
     ) {
+        if (parentView == null) return
+
         lastSourceCenterX = sourceX
         lastSourceCenterY = sourceY
+
         val location = IntArray(2)
-        mView.getLocationInWindow(location)
-        val viewX = location[0]
-        val viewY = location[1]
-        val decorViewWidth = mDecorView.measuredWidth
-        val decorViewHeight = mDecorView.measuredHeight
-        val x = viewX.toFloat()
-        val cardWidth = imageWidth
-        val cardX = x + destinationX - cardWidth / 2.0f
-        val y = viewY.toFloat() + destinationY - imageHeight / 2.0f
-        if (cardX < 0.0f) {
-            mCardView.x = 0.0f
-        } else {
-            val rightBoundary = decorViewWidth.toFloat()
-            if (cardX > rightBoundary - cardWidth) {
-                mCardView.x = rightBoundary - cardWidth
-            } else {
-                mCardView.x = cardX
-            }
+        sourceView.getLocationInWindow(location)
+
+        val cardX = (location[0] + destinationX - magnifierWidth / 2.0f).coerceIn(0f, (parentView.width - magnifierWidth))
+        val cardY = (location[1] + destinationY - magnifierHeight / 2.0f).coerceIn(0f, (parentView.height - magnifierHeight))
+
+        magnifierCard.x = cardX
+        magnifierCard.y = cardY
+
+        if (magnifierCard.visibility != View.VISIBLE) {
+            magnifierCard.visibility = View.VISIBLE
         }
-        if (y < 0.0f) {
-            mCardView.y = 0.0f
-        } else {
-            val bottomBoundary = decorViewHeight.toFloat()
-            val cardHeight = imageHeight
-            if (y > bottomBoundary - cardHeight) {
-                mCardView.y = bottomBoundary - cardHeight
-            } else {
-                mCardView.y = y
-            }
-        }
-        mCardView.visibility = View.VISIBLE
         update()
     }
 
-    private fun update() {
-        mView.isDrawingCacheEnabled = true
-        val viewBitmap = mView.drawingCache.copy(Bitmap.Config.ARGB_8888, true)
-        mView.isDrawingCacheEnabled = false
-        var width = imageWidth
-        var zoom = zoom
-        width /= zoom
-        zoom = imageHeight / zoom
-        val lastBitmap = mLastBitmap
+    fun dismiss() {
+        magnifierCard.visibility = View.GONE
         lastBitmap?.recycle()
-        val croppedBitmap = cropBitmap(
-            viewBitmap,
-            (lastSourceCenterX - width / 2.0f).toInt(),
-            (lastSourceCenterY - zoom / 2.0f).toInt(),
-            width.toInt(),
-            zoom.toInt()
-        )
-        mLastBitmap = croppedBitmap
-        mImageView.setImageBitmap(croppedBitmap)
+        lastBitmap = null
     }
 
-    var elevation: Float
-        get() = mElevation
-        set(elevation) {
-            mElevation = elevation
-            mCardView.cardElevation = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                elevation,
-                displayMetrics
-            )
+    fun setDimensions(width: Float, height: Float) {
+        magnifierWidth = width
+        magnifierHeight = height
+        magnifierCard.layoutParams.width = width.toInt()
+        magnifierCard.layoutParams.height = height.toInt()
+        magnifierCard.requestLayout()
+    }
+
+    private fun update() {
+        // If window is null, we cannot perform the copy.
+        // The init block should prevent this, but it's good practice to check.
+        val window = this.window ?: return
+
+        val srcWidth = (magnifierWidth / zoom)
+        val srcHeight = (magnifierHeight / zoom)
+
+        val srcLeft = lastSourceCenterX - srcWidth / 2f
+        val srcTop = lastSourceCenterY - srcHeight / 2f
+
+        val viewLocation = IntArray(2)
+        sourceView.getLocationInWindow(viewLocation)
+
+        val srcRect = Rect(
+            (viewLocation[0] + srcLeft).toInt(),
+            (viewLocation[1] + srcTop).toInt(),
+            (viewLocation[0] + srcLeft + srcWidth).toInt(),
+            (viewLocation[1] + srcTop + srcHeight).toInt()
+        )
+
+        // Ensure source rectangle has a non-zero size
+        if (srcRect.width() <= 0 || srcRect.height() <= 0) {
+            return
         }
+
+        val bitmap = createBitmap(srcRect.width(), srcRect.height())
+
+        PixelCopy.request(window, srcRect, bitmap, { result ->
+            if (result == PixelCopy.SUCCESS) {
+                mainHandler.post {
+                    lastBitmap?.recycle()
+                    lastBitmap = bitmap
+                    magnifierImage.setImageBitmap(lastBitmap)
+                }
+            } else {
+                bitmap.recycle()
+            }
+        }, mainHandler)
+    }
 }
